148d6f3a1b78f00fc813997de9ed405c52f6d0ff
YuyaoYan <32066567+YuyaoYan@users.noreply.github.com>
2018-03-29 15:33:10 +0800

    优化界面

--- WEB/js/cloud.js
+++ WEB/js/cloud.js
@@ -2,8 +2,9 @@
 // Cloud Float...
     var $main = $cloud = mainwidth = null;
     var offset1 = 450;
-	var offset2 = 0;
-    var offset3 = 120;
+	var offset2 = 300;
+    var offset3 = 800;
+    var offset4 = 0;
 	
 	var offsetbg = 0;
     
@@ -14,6 +15,7 @@
             $cloud1 = $("#cloud1");
 			$cloud2 = $("#cloud2");
             $cloud3 = $("#cloud3");
+            $cloud4 = $("#cloud4");
 			
             mainwidth = $main.outerWidth();
          
@@ -33,15 +35,23 @@
         if (offset3 >= mainwidth) {
              offset3 =  -580;
         }
+
+        if (offset4 >= mainwidth) {
+             offset4 =  -580;
+        }
 		
-        offset1 += 1.1;
+        offset1 += 0.9;
 		offset2 += 1;
-        offset3 += 3;
+        offset3 += 1.3;
+        offset4 += 0.8;
         $cloud1.css("background-position", offset1 + "px 100px")
 		
 		$cloud2.css("background-position", offset2 + "px 460px")
 
-        $cloud3.css("background-position", offset3 + "px 460px")
+        $cloud3.css("background-position", offset3 + "px 200px")
+
+        $cloud4.css("background-position", offset4 + "px 300px")
+
     }, 70);
 	
 	

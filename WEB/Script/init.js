9e81c52ad91e37b48b14aa8959d995fcb220cf87
Kingder <zj809667533@live.com>
2018-03-29 09:54:01 +0800

    修复冲突

--- WEB/Script/init.js
+++ WEB/Script/init.js
@@ -1,18 +1,4 @@
 $(function(){
-<<<<<<< HEAD
-    AppStart();
-})
-//应用程序开始
-function AppStart(){
-    startMap();
-    eventBand();
-
-}
-//初始化百度地图
-function startMap(){
-	// 百度地图API功能
-	map = new BMap.Map("allmap");    // 创建Map实例(全局)
-=======
     StartApp();
 })
 
@@ -25,7 +11,6 @@ function StartApp(){
 function initMap(){
     	// 百度地图API功能
 	var map = new BMap.Map("allmap");    // 创建Map实例
->>>>>>> 95d2e974901af009686a72502e37523279b7d488
 	map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
 	//添加地图类型控件
 	map.addControl(new BMap.MapTypeControl({
@@ -34,16 +19,6 @@ function initMap(){
             BMAP_HYBRID_MAP
         ]}));	  
 	map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-<<<<<<< HEAD
-	map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
-}
-//绑定按钮点击事件
-function eventBand(){
-    //绑定雨量站事件
-    $('#rainFallShow').click(function(){
-        rainClass.getData();
-      });
-=======
 	map.enableScrollWheelZoom(true);  
 }
 function initEvent(){
@@ -58,5 +33,4 @@ $("#rainFallShow").click(function(){
     }
 
 })
->>>>>>> 95d2e974901af009686a72502e37523279b7d488
 }

beb83bb0144ec62b1a57b0a823850a265cd18aa2
Kingder <zj809667533@live.com>
2018-03-30 08:48:23 +0800

    实现雨量站功能，重构雨量站类。重建菜单
    实现雨量站功能，重构雨量站类。重建菜单

--- WEB/Script/function.js
+++ WEB/Script/function.js
@@ -1,9 +1,14 @@
-//显示雨量站
+/*
+描述：关于雨量站的相关功能
+功能：获取数据，添加标注，清楚数据
+作者：kingder
+时间：2018/03/29
+*/
 var rainClass={
-    addMark:function(x,y,info){
+    addMark:function(x,y,info,data,id){
         var sContent ='<button type="button" id="myButton" data-loading-text="Loading..." class="btn btn-primary" autocomplete="off">Loading state</button>';
         var point = new BMap.Point(x, y);
-        var myIcon = new BMap.Icon("http://lbsyun.baidu.com/jsdemo/img/fox.gif", new BMap.Size(300,157));
+        var myIcon = new BMap.Icon("images/symble/rain1.png", new BMap.Size(50,50));
         var marker = new BMap.Marker(point,{icon:myIcon});
         var infoWindow = new BMap.InfoWindow(sContent);
         map.addOverlay(marker);
@@ -17,14 +22,28 @@ var rainClass={
     },
     getData:function(){
         var data=[];
+        //TODO:hou
+        data.push({'x':116.417,'y':39.909,'info':'天安门积水点','data':0,'id':'w01'});
+        data.push({'x':116.441063,'y':39.954548,'info':'东直门积水点','data':0,'id':'w02'});
 
         //获取数据传入回调
         this.callBack(data);
 
     },
     callBack:function(data){
-        this.addMark(116.417, 39.909,"");
+        for(var i=0;i<data.length;i++)
+          this.addMark(data[i].x, data[i].y,data[i].info,data[i].data,data[i].id);
 
+    },
+    hideAll:function(){
+        map.clearOverlays();  
     }
 
+}
+
+var mapTool={
+    hideAll:function(){
+        map.clearOverlays(); 
+
+    }
 }
--- WEB/Script/init.js
+++ WEB/Script/init.js
@@ -1,36 +1,44 @@
-$(function(){
+$(function () {
     StartApp();
 })
 
-function StartApp(){
+function StartApp() {
     initMap()
     initEvent()
 
 }
 
-function initMap(){
-    	// 百度地图API功能
-	var map = new BMap.Map("allmap");    // 创建Map实例
-	map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
-	//添加地图类型控件
-	map.addControl(new BMap.MapTypeControl({
-		mapTypes:[
+function initMap() {
+    // 百度地图API功能
+    map = new BMap.Map("allmap");    // 创建Map实例（强制全局）
+    map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);  // 初始化地图,设置中心点坐标和地图级别
+    //添加地图类型控件
+    map.addControl(new BMap.MapTypeControl({
+        mapTypes: [
             BMAP_NORMAL_MAP,
             BMAP_HYBRID_MAP
-        ]}));	  
-	map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
-	map.enableScrollWheelZoom(true);  
+        ]
+    }));
+    map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
+    map.enableScrollWheelZoom(true);
+    document.getElementById("allmap").style.height=document.body.clientHeight-51+"px";
 }
-function initEvent(){
-$("#rainFallShow").click(function(){
-    if($("#rainFallShow").hasClass("active")){
-        //隐藏
-        $("#rainFallShow").removeClass();
-    }
-    else{
-        //显示
-        $("#rainFallShow").addClass("active");
-    }
+function initEvent() {
+    $("#rainFallShow").click(function () {
+        if ($("#rainFallShow").hasClass("active")) {
+            //隐藏
+            $("#rainFallShow").removeClass();
+            mapTool.hideAll();
 
-})
+        }
+        else {
+            mapTool.hideAll();
+
+            //显示
+            $("#rainFallShow").addClass("active");
+            rainClass.getData();
+
+        }
+
+    })
 }

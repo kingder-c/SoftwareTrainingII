01f11e4bf4b7140b0f4b2cd9652d24059256b9cb
snowIceg <gxbkss@163.com>
2018-05-03 16:41:02 +0800

    创建监测值表

--- DataBase/software_training.sql
+++ DataBase/software_training.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50721
  File Encoding         : utf-8
 
- Date: 05/03/2018 15:50:00 PM
+ Date: 05/03/2018 16:40:28 PM
 */
 
 SET NAMES utf8;
@@ -26,6 +26,17 @@ CREATE TABLE `tbl_monitoring_point_info` (
   `MONITORINGNAME` varchar(50) DEFAULT NULL COMMENT '监测点名称',
   `LOCATION` varchar(50) DEFAULT NULL COMMENT '位置',
   `MONITORINGTYPE` varchar(50) DEFAULT NULL COMMENT '类型',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `tbl_monitoring_value`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_monitoring_value`;
+CREATE TABLE `tbl_monitoring_value` (
+  `ID` int(50) NOT NULL AUTO_INCREMENT COMMENT '监测点ID',
+  `MONITORINGVALUE` varchar(50) DEFAULT NULL COMMENT '检测值',
+  `UPDATETIME` datetime NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 

e73e688cc39eca1c27c2ca5e8f81d83c70c8caf8
snowIceg <gxbkss@163.com>
2018-05-03 16:45:34 +0800

    创建监测历史表

--- DataBase/software_training.sql
+++ DataBase/software_training.sql
@@ -11,11 +11,22 @@
  Target Server Version : 50721
  File Encoding         : utf-8
 
- Date: 05/03/2018 16:40:28 PM
+ Date: 05/03/2018 16:45:11 PM
 */
 
 SET NAMES utf8;
 SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `tbl_monitoring_history`
+-- ----------------------------
+DROP TABLE IF EXISTS `tbl_monitoring_history`;
+CREATE TABLE `tbl_monitoring_history` (
+  `ID` int(50) NOT NULL AUTO_INCREMENT COMMENT '监测点ID',
+  `MONITORINGVALUE` varchar(50) DEFAULT NULL COMMENT '检测值',
+  `UPDATETIME` datetime NOT NULL COMMENT '更新时间',
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 --  Table structure for `tbl_monitoring_point_info`
